3ccc6e026cfa70272e97bdbec603859f7aa887cb
AzizbekAzimjonovich <azizbekazimjonovich@gmail.com>
2024-01-24 09:39:20 +0500

    xvariant

---
+++ index.js
@@ -0,0 +1,6 @@
+const button=document.getElementById('button');
+const form=document.querySelector('#form');
+const name=document.querySelector('#name');
+const price=document.querySelector('#price');
+const category=document.querySelector('#category');
+const tbody=document.querySelector('#tbody');
